31a392e4885837c0730ffe4edd19bced7ba36621
BrennoBener <brennobbener@live.com>
2021-08-27 13:46:22 -0300

    parte 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggel')
+      navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+  navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+  })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
