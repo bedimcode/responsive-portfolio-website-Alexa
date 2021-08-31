122b29bcac0c96cfa91440acc6b19046cf9b2630
BrennoBener <brennobbener@live.com>
2021-08-30 23:36:52 -0300

    varias abas, menus, botoes e terminei com interacao do menu de habilidades

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,23 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHeader = document.querySelectorAll('skills_header')
 
+function toggleSkills(){
+  let itemClass = this.parentNode.className
+
+  for(i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills_content skills_close'
+  }
+  if(itemClass === 'skills_content skills_close'){
+    this.parentNode.className = 'skills_content skills_open'
+  }
+}
+
+skillsHeader.forEach((el) =>{
+  el.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
