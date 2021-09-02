e9ac296408a205338adebd31011a3c499e8d83f9
Raju_Ahmed <rajuahmad6789@gmail.com>
2021-09-03 02:31:22 +0600

    feature portfolio section complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,7 +94,18 @@ modalCloses.forEach(modalClose => {
 });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
