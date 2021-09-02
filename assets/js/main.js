23d20a34672cc279699f92f122e644efa470ae2f
Raju_Ahmed <rajuahmad6789@gmail.com>
2021-09-03 01:39:20 +0600

    feature services and related modal added

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,27 @@ tabs.forEach(tab =>{
 })
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close');
 
+let modal = function (modalClick){
+    modalViews[modalClick].classList.add('active-modal');
+};
+
+modalBtns.forEach((modalBtn, i)=>{
+    modalBtn.addEventListener('click', ()=>{
+        modal(i);
+    })
+});
+
+modalCloses.forEach(modalClose => {
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal');
+        });
+    });
+});
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
