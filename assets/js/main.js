c5bf020de0a1c2ec616c82269d5527415cfff03a
Raju_Ahmed <rajuahmad6789@gmail.com>
2021-09-02 19:30:47 +0600

    feature qualifications added

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,25 @@ skillsHeader.forEach((el) => {
 });
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active');
+        });
+
+        target.classList.add('qualification__active');
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active');
+        })
+        tab.classList.add('qualification__active');
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
