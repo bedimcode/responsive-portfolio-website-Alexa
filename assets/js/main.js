52176e69e3c42b7a97f1a8be5e7b9d1e0eec72da
BrennoBener <brennobbener@live.com>
2021-09-03 15:37:14 -0300

    habilidades completo proximo passo - Qualificaçoes

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,23 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 const sections = document.querySelectorAll('section[id]')
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+  let itemClass = this.parentNode.className
+
+  for(i = 0; i < skillsContent.length; i++){
+    skillsContent[i].className = 'skills__content skills__close'
+  }
+  if(itemClass === 'skills__content skills__close'){
+    this.parentNode.className = 'skills__content skills__open'
+  }
+}
+
+skillsHeader.forEach((el) =>{
+  el.addEventListener('click', toggleSkills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
