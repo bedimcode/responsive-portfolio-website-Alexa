747cc9f6953238d81abde945fc9c55c5984c4e01
emiquesorito <128050217+emiquesorito@users.noreply.github.com>
2023-08-28 08:16:37 -0400

    Update main.js
    Added functionality to hide the nav when a link is clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,30 @@ const navMenu = document.getElementById('nav-menu'),
   navClose = document.getElementById('nav-close')
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
 if(navToggle){
   navToggle.addEventListener('click', () =>{
     navMenu.classList.add('show-menu')
   })
 }
 
-/*==================== REMOVE MENU MOBILE ====================*/
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+  navClose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu')
+  })
+}
 
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+  const navMenu = document.getElementById('nav-menu')
+  // When we click on each nav_link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
