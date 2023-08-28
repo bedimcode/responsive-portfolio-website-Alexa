0daf27178acfe409b977c22bb43618e5e12ca16b
emiquesorito <128050217+emiquesorito@users.noreply.github.com>
2023-08-28 08:10:11 -0400

    Update main.js
    Add get and event listener for showing menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,18 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
-
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+  navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+  })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
