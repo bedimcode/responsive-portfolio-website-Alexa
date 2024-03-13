40301f9e67d49232641180a9603a5c74bd7c7c0e
Sirius Ganesh <stunfisk@Stunfisk.local>
2024-03-13 11:13:58 -0500

    03132024_morning

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,14 @@ if(navClose){
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
